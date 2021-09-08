022ded3420bddabaf5ca5a71ed690b4650c1afa1
Gregor Pogacnik <1640719+fiksn@users.noreply.github.com>
2021-09-08 16:39:04 +0200

    Disablemod

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
-	# security-nix ![Build](https://github.com/fiksn/security-nix/actions/workflows/build.yaml/badge.svg) [![tippin.me](https://badgen.net/badge/%E2%9A%A1%EF%B8%8Ftippin.me/@fiksn/F0918E)](https://tippin.me/@fiksn)
+# security-nix ![Build](https://github.com/fiksn/security-nix/actions/workflows/build.yaml/badge.svg) [![tippin.me](https://badgen.net/badge/%E2%9A%A1%EF%B8%8Ftippin.me/@fiksn/F0918E)](https://tippin.me/@fiksn)
 A collection of [nix](https://nixos.org/) modules for running a secure NixOS server
+
+You might want to import:
+```
+<nixpkgs/nixos/modules/profiles/hardened.nix>
+```
 
 ## Hostfw
 
 Hostfw is a module for managing the firewall. Using this simple abstraction you can open a certain UDP or TCP port for a list of trusted IPs instead of
 having to call low-level code (like directly calling `iptables` or modifying `networking.firewall.extraCommands`)
 
-### Usage:
+### Usage
 
 ```
 imports = [ ./hostfw.nix ];
@@ -21,5 +26,18 @@ services.hostfw = {
 };
 ```
 
+## Disablemod
 
+I had trouble with `security.lockKernelModules`. Without that set to false `boot.blacklistedKernelModules` does not prevent
+a particular module to be loaded.
+
+### Usage
+
+```
+import = [ ./disablemod.nix ];
+services.disablemod = {
+  enable = true;
+  modules = services.disablemod.cisRecommendedModules;
+};
+```
 
---
+++ modules/disablemod.nix
@@ -0,0 +1,57 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+let
+  cfg = config.services.disablemod;
+  cisRecommendation = [ "freevxfs" "jffs2" "hfs" "hfsplus" "udf" "sctp" "dccp" "rds" "tipc" ];
+in
+{
+  options.services.disablemod = {
+    enable = mkEnableOption {
+      type = types.bool;
+      default = false;
+      description = ''
+        Enable disablemod
+      '';
+    };
+
+    modules = mkOption {
+      type = types.listOf types.str;
+      default = config.boot.blacklistedKernelModules;
+      example = [ "cirrusfb" "i2c_piix4" ];
+      description = ''
+        List of names of kernel modules that should not be loaded
+        automatically by the hardware probing code.
+      '';
+    };
+
+    cisRecommendedModules = mkOption {
+      type = types.listOf types.str;
+      readOnly = true;
+      description = ''
+        Read-only list of names of kernel modules that should not be loaded
+        (defaults from CIS benchmark)
+      '';
+    };
+
+  };
+
+  config = {
+    # Always available
+    services.disablemod.cisRecommendedModules = cisRecommendation;
+
+    # Only when cfg.enable == true (some ugly trickery)
+    security.lockKernelModules = mkIf cfg.enable (mkOverride false);
+    environment.etc."modprobe.d/disablemod.conf".text =
+      if cfg.enable then
+        ''
+          ${flip concatMapStrings cfg.modules (name: ''
+            install ${name} /bin/true
+          '')}
+          ${
+            # Noop - not sure how to else force evaluation, tried builtins.seq, without this readOnly will have no effect
+            if length cfg.cisRecommendedModules > 0 then "" else ""
+          }
+        '' else "";
+  };
+}
