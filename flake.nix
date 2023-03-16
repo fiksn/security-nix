bfd822df25188ba9b01066e7f9b65d0fa1654fea
Gregor Pogacnik <gregor@bolt.observer>
2023-03-16 13:43:33 +0100

    build: and update version too

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs/release-21.11";
+    nixpkgs.url = "github:nixos/nixpkgs/release-22.11";
   };
 
   outputs = { self, flake-utils, nixpkgs }:
